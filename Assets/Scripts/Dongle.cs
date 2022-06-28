052b76233780479ee9b01b6d5d8e7dabc8824b9f
baejichul <jayseebae@naver.com>
2022-06-28 14:12:03 +0900

    0628_002
    0628_002

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 
 
-namespace MyCat
+
+public class Dongle : MonoBehaviour
 {
-    public class Dongle : MonoBehaviour
+    public bool _isDrag;
+    Rigidbody2D _rigid;
+
+
+    void Awake()
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
             
-        }
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isDrag)
             MoveMouse();
-        }
+    }
 
-        void MoveMouse()
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0.0f;
-            transform.position = Vector3.Lerp(transform.position, mousePos, 0.01f);
-        }
+    void MoveMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+        // x축 이동제한
+        float leftBorder   = -4.2f + transform.localScale.x / 2f;
+        float righttBorder = 4.2f - transform.localScale.x / 2f;
+
+        if (mousePos.x < leftBorder)
+            mousePos.x = leftBorder;
+        else if (mousePos.x > righttBorder)
+            mousePos.x = righttBorder;
+
+        mousePos.y = 8.0f;      // y축 고정
+        mousePos.z = 0.0f;      // z축 고정
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+    }
+
+    public void Drag()
+    {
+        _isDrag = true;
+    }
+
+    public void Drop()
+    {
+        _isDrag = false;
+        _rigid.simulated = true;
     }
 }
